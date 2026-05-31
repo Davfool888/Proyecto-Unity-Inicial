de26eed4d435ff904442a25ff4fec5982d5403f9
Davfool888 <davfool888@gmail.com>
2026-05-30 22:17:31 -0500

    Creando nuevos scrips para el manejo de las scenas

--- Assets/scripts/Board.cs
+++ Assets/scripts/Board.cs
@@ -8,11 +8,27 @@ public class Board: MonoBehaviour
     public int width;
     public int height;
     public GameObject tileObject;
+    public float cameraSizeOffset;
+    public float cameraVerticalOffset;
 
 
     void Start()
     {
         SetupBoard();
+        PositionCamera();
+    }
+
+    private void PositionCamera()
+    {
+        float newPosX = (float)width / 2f;
+        float newPosY = (float)height / 2f;
+
+        Camera.main.transform.position = new Vector3(newPosX - 0.5f, newPosY - 0.5f + cameraVerticalOffset, -10f);
+
+        float horizontal = width + 1;
+        float vertical = (height / 2) + 1;
+
+        Camera.main.orthographicSize = horizontal > vertical ? horizontal + cameraSizeOffset : vertical + cameraSizeOffset;
     }
 
     private void SetupBoard()
@@ -23,6 +39,7 @@ public class Board: MonoBehaviour
             {
                 var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
                 o.transform.parent = transform;
+                o.GetComponent<Tile>()?.Setup(x, y, this);
             }   
         }
     }
