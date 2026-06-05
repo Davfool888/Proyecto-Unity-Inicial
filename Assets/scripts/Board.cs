dea7042a13a97303bfe6a345580131506d92e318
Davfool888 <davfool888@gmail.com>
2026-06-04 23:59:00 -0500

    Añadiendo funcionalidades de movimientos entre tiles, que no se puedan mover mas de una casilla

--- Assets/scripts/Board.cs
+++ Assets/scripts/Board.cs
@@ -1,7 +1,9 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Board: MonoBehaviour
+public class Board : MonoBehaviour
 {
 
 
@@ -20,7 +22,7 @@ public class Board: MonoBehaviour
     Piece[,] Pieces;
 
     Tile startTile;
-    Tile endTile; 
+    Tile endTile;
 
 
     void Start()
@@ -66,7 +68,7 @@ public class Board: MonoBehaviour
 
     private void SetupBoard()
     {
-        for(int x  = 0; x < width; x++)
+        for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
@@ -74,7 +76,7 @@ public class Board: MonoBehaviour
                 o.transform.parent = transform;
                 Tiles[x, y] = o.GetComponent<Tile>();
                 Tiles[x, y]?.Setup(x, y, this);
-            }   
+            }
         }
     }
 
@@ -90,7 +92,7 @@ public class Board: MonoBehaviour
 
     public void TileUp(Tile tile_)
     {
-        if(startTile!= null && endTile != null)
+        if (startTile != null && endTile != null && IsCloaseTo(startTile, endTile))
         {
             SwapTiles();
         }
@@ -108,5 +110,87 @@ public class Board: MonoBehaviour
 
         Pieces[startTile.x, startTile.y] = EndPiece;
         Pieces[endTile.x, endTile.y] = StarPiece;
+    }
+
+
+    public bool IsCloaseTo(Tile start, Tile end)
+    {
+        if (Math.Abs((start.x - end.x)) == 1 && start.y == end.y)
+        {
+            return true;
+        }
+
+        if (Math.Abs((start.y - end.y)) == 1 && start.x == end.x)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public List<Piece> GetMatchByDirection(int xpos, int ypos, Vector2 direction, int minPieces = 3)
+    {
+        List<Piece> matches = new List<Piece>();
+        Piece startPiece = Pieces[xpos, ypos];
+        matches.Add(startPiece);
+
+        int nextX;
+        int nextY;
+        int MaxVal = width > height ? width : height;
+
+        for (int i = 1; i > MaxVal; i++)
+        {
+            nextX = xpos + ((int)direction.x * i);
+            nextY = ypos + ((int)direction.y * i);
+            if (nextX >= 0 && nextX < width && nextY >= 0 && nextY < height)
+            {
+                var nextPiece = Pieces[nextX, nextY];
+                if (nextPiece != null && nextPiece.pieceType == startPiece.pieceType)
+                {
+                    matches.Add(nextPiece);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        if (matches.Count >= minPieces)
+        {
+            return matches;
+        }
+
+        return null;
+    }
+
+
+    public List<Piece> GetMatchByPiece(int xpos, int ypos, int minPieces = 3)
+    {
+        var upMatchs = GetMatchByDirection(xpos, ypos, new Vector2(0, 1), 2);
+        var downMatchs = GetMatchByDirection(xpos, ypos, new Vector2(0, -1), 2);
+        var rightMatchs = GetMatchByDirection(xpos, ypos, new Vector2(1, 0), 2);
+        var leftMatchs = GetMatchByDirection(xpos, ypos, new Vector2(-1, 0), 2);
+
+        if (upMatchs == null) upMatchs = new List<Piece>();
+        if (downMatchs == null) downMatchs = new List<Piece>();
+        if (rightMatchs == null) rightMatchs = new List<Piece>();
+        if (leftMatchs == null) leftMatchs = new List<Piece>();
+
+        var verticalMatches = upMatchs.Union(downMatchs).ToList();
+        var horizontalMatches = leftMatchs.Union(rightMatchs).ToList();
+
+        var foundMatches = new List<Piece>();
+
+        if(verticalMatches.Count >= minPieces)
+        {
+            foundMatches = foundMatches.Union(verticalMatches).ToList();
+        }
+
+        if (horizontalMatches.Count >= minPieces)
+        {
+            foundMatches = foundMatches.Union(horizontalMatches).ToList();
+        }
+
+        return foundMatches;
     }
 }
